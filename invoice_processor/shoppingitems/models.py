6efa72954de30af8dac437b65b4b9216cea27df9
jennguyen <jennguyen.datascience@gmail.com>
2024-04-17 00:59:40 +0800

    feat: Update models

--- invoice_processor/shoppingitems/models.py
+++ invoice_processor/shoppingitems/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 
-class ShoppingItems(models.Model):
-    brand = models.CharField(max_length=50)
-    name = models.CharField(max_length=255)
-    weight = models.DecimalField(max_digits=3, decimal_places=2)
-    price_aud = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return self.name
-
-
-class Brands(models.Model):
+class Brand(models.Model):
         BRAND_CATEGORIES = [
              ('Vitamins', 'Vitamins'),
              ('Beauty', 'Beauty'),
@@ -24,3 +14,22 @@ class Brands(models.Model):
 
         def __str__(self):
              return f'{self.name} ({self.get_category_display()})'
+
+
+class Product(models.Model):
+     name = models.CharField(max_length=255, unique=True)
+     brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True)
+     weight = models.DecimalField(max_digits=5, decimal_places=2)
+
+     def __str__(self):
+          return f'{self.name} {self.weight}'
+
+
+class Transaction(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price_aud = models.DecimalField(max_digits=10, decimal_places=2)
+    purchase_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
