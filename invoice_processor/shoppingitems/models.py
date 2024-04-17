e0c1eb6be908bf9ed79eac0443d7ff65684b140b
jennguyen <jennguyen.datascience@gmail.com>
2024-04-18 00:44:48 +0800

    feat: Remove brands out of Products table

--- invoice_processor/shoppingitems/models.py
+++ invoice_processor/shoppingitems/models.py
@@ -18,7 +18,6 @@ class Brand(models.Model):
 
 class Product(models.Model):
      name = models.CharField(max_length=255, unique=True)
-     brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True)
      weight = models.DecimalField(max_digits=5, decimal_places=2)
 
      def __str__(self):
@@ -27,6 +26,7 @@ class Product(models.Model):
 
 class Transaction(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField()
     price_aud = models.DecimalField(max_digits=10, decimal_places=2)
     purchase_date = models.DateTimeField(auto_now_add=True)
